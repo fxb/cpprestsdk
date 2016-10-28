18b21af87bef0fa6a7fe21eac1fe9303a0b4d8dc
Felix Bruns <felixbruns@spotify.com>
2016-10-28 16:59:47 +0200

    Prevent re-definition of NOMINMAX.
    This is often already defined on the compiler command-line
    and a re-definition will result in a warning / error.

--- Release/src/pch/stdafx.h
+++ Release/src/pch/stdafx.h
@@ -51,7 +51,9 @@
 
 #ifndef WIN32_LEAN_AND_MEAN
 #define WIN32_LEAN_AND_MEAN             // Exclude rarely-used stuff from Windows headers
-// Windows Header Files:
+#endif
+
+#ifndef NOMINMAX
 #define NOMINMAX
 #endif
 
